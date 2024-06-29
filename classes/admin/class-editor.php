80348c86449defbb02a2f86f79089cebcda58d94
Erik Larson <eriklarsonco@gmail.com>
2024-06-29 17:55:07 -0400

    fixed editor config

--- classes/admin/class-editor.php
+++ classes/admin/class-editor.php
@@ -5,15 +5,29 @@ class EditorConfig extends Base
 {
     /**
      * constructor
-     *
-     * @param boolean $static
      */
-    public function __construct($static = false)
+    public function __construct()
     {
-        if (!$static) {
-            // restore classic text editor by disabling gutenberg editor
-            add_filter('use_block_editor_for_post', '__return_false');
+        // restore classic text editor by disabling gutenberg editor
+        add_filter('use_block_editor_for_post', '__return_false');
+
+        add_filter('user_can_richedit', [$this, 'disableVisualEditor']);
+    }
+
+    /**
+     * remove wysiwyg editor from text editor
+     *
+     * @param boolean $enabled
+     * @return boolean
+     */
+    public function disableVisualEditor($enabled)
+    {
+        global $post;
+
+        if ($post->post_type === parent::formatLabel('html snippet')) {
+            return false;
         }
+        return $enabled;
     }
 }
 
